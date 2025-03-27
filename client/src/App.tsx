3def14de1f4d9674c91515160faaa825c3052a6a
Bojan Jankov <jankovbojan4@gmail.com>
2025-03-27 19:23:51 +0100

    added client front-end side with React and Tailwind seutped

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="mb-5 font-bold flex-items">Barbers App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
